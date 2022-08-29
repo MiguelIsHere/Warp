1f664ac2d618e1fd9ed329f60f64653f72d78975
Benjamin Low <benjaminlow121@gmail.com>
2022-08-29 22:31:25 +0800

    Revert "Character Movement"
    This reverts commit 9ed68383cde84590a80dd30a0d493600189570ae, reversing
    changes made to b116fa3b86b28da6850952c4d59fbed62206c051.

--- Warp/Assets/Scripts/C#/Miguel'sScripts/CameraController.cs
+++ Warp/Assets/Scripts/C#/Miguel'sScripts/CameraController.cs
@@ -8,7 +8,7 @@ public class CameraController : MonoBehaviour
     public GameObject player1, player2; // Stores references to the two players
 
     public GameObject focusPoint; // The camera will follow this gameObject, not either of the players under normal conditions
-    public GameObject target; // Allows camera to toggle between following the focus to following a player by switching this
+    [HideInInspector] public GameObject target; // Allows camera to toggle between following the focus to following a player by switching this
 
     [Header("Variables")]
     public float calculatedSize;
@@ -16,16 +16,13 @@ public class CameraController : MonoBehaviour
     public float zoomSize; // This value is used instead of minimumSize when a player is killed to create a kill camera.
     public float distance;
 
-    public bool isZooming;
-
     Camera theCamera;
     // Start is called before the first frame update
     void Start()
     {
-        theCamera = GetComponent<Camera>(); 
-        SoundManager.instance.Play("LevelMusic"); // Start playing the level BGM and stop playing the menu BGM
-        SoundManager.instance.StopPlaying("MenuMusic");
-        focusPoint = GameObject.Find("FocusPoint");
+        theCamera = GetComponent<Camera>();
+
+        focusPoint = GameObject.Find("Target");
         target = focusPoint;
 
     }
@@ -35,98 +32,41 @@ public class CameraController : MonoBehaviour
     {
 
         Vector3 v = (player1.transform.position + player2.transform.position) / 2f; // Calculates the midpoint between the two players
+        distance = Vector3.Distance(player1.transform.position, player2.transform.position); // Calculates distance between the players
 
-        // Calculate cameraSize to use
-        Vector3 p1 = new Vector3(player1.transform.position.x, 0, player1.transform.position.z);
-        Vector3 p2 = new Vector3(player2.transform.position.x, 0, player2.transform.position.z);
-
-        distance = Vector3.Distance(p1, p2); // Calculates distance between the players
-        calculatedSize = distance * 0.8f;
-
-        focusPoint.transform.position = v; // Sets the position of the focusPoint to this position
-
-        if (isZooming) return; // If we are zooming in during the deathCam, do not set position of the camera in the transform
-
-        float s = Mathf.Clamp(calculatedSize, minimumSize, calculatedSize);
-        theCamera.orthographicSize = Mathf.Lerp(theCamera.orthographicSize, s, Time.deltaTime * 4f);
+        calculatedSize = distance * 1.20f;
+        theCamera.orthographicSize = Mathf.Clamp(calculatedSize, minimumSize, calculatedSize);
         // If calculatedSize is greater than minimumSize, use the original value. If calculatedSize is smaller than minimumSize, use minimumSize instead.
 
-        Vector3 d = new Vector3(
+        focusPoint.transform.position = v; // Sets the position of the focus of camera to this position
+        transform.position = new Vector3(
             focusPoint.transform.position.x + (-17.6f),
-            21f, //focusPoint.transform.position.y + 21f, y-value is constant so camera doesnt follow player or change size down when they die.
+            focusPoint.transform.position.y + 21f,
             focusPoint.transform.position.z + (-16.5f)); // Move camera to 
 
-        // Smoothly lerp to focusPoint's position.
-        transform.position = Vector3.Lerp(transform.position, d, Time.deltaTime * 3f);
-
+        //if (player1.GetComponent<PlayerCube>().isDead || player2.GetComponent<PlayerCube>().isDead) // If either player has died
+        //{
+        //    target = player
+        //    StartCoroutine(DeathCam());
+        //}
     }
 
     public IEnumerator DeathCam(GameObject zoomTarget)
     {
-        if (isZooming) yield break;
-        float t = 0f;
-        float duration = 3f; //float duration = 300
+        float t = 0;
+        float duration = 1;
 
-        target = zoomTarget;
-        isZooming = true;
-
-        //bool isZoomingOut = false;
         Vector3 destination = new Vector3(
             zoomTarget.transform.position.x + (-17.6f),
-            21f, //zoomTarget.transform.position.y + 21f,
+            zoomTarget.transform.position.y + 21f,
             zoomTarget.transform.position.z + (-16.5f)); // The position to lerp towards, this is a point above the dead player
 
-
         while (t < duration)
         {
-            //Debug.Log("Lerping");
-            //if (isZoomingOut == false) 
-            transform.position = Vector3.Lerp(transform.position, destination, t / duration / 2); // Moves the camera over to the dead player
-            theCamera.orthographicSize = Mathf.Lerp(theCamera.orthographicSize, zoomSize, t / duration / 2); // Zoom in the camera on the player's corpse
+            transform.position = Vector3.Lerp(transform.position, destination, t / duration); // Moves the camera over the dead player
+            theCamera.orthographicSize = Mathf.Lerp(theCamera.orthographicSize, zoomSize, t / duration); // Zoom in the camera on the player's corpse
 
-            t += Time.deltaTime;
             yield return new WaitForEndOfFrame();
         }
-
-        //yield return new WaitUntil(() => transform.position == destination); // Once the camera has reached the destination, zoom back out
-        yield return new WaitForEndOfFrame();
-
-        target = focusPoint; // Set the target back to the focusPoint of the camera, which is midpoint between two players
-
-        destination = new Vector3(
-            target.transform.position.x + (-17.6f),
-            21f,
-            target.transform.position.z + (-16.5f)); // Change destination to be based off focusPoint position
-
-        t = 0f; // Reset t
-
-        //Return camera to target position and regular size
-        while (t < duration)
-        {
-            transform.position = Vector3.Lerp(transform.position, destination, (t / duration) / 2);
-
-            float s = Mathf.Clamp(calculatedSize, minimumSize, calculatedSize); // If the calculated size is smaller than minimum size, use minimum size
-            theCamera.orthographicSize = Mathf.Lerp(theCamera.orthographicSize, s, (t / duration) / 2);
-
-            t += Time.deltaTime;
-            yield return new WaitForEndOfFrame();
-        }
-
-        // Revive and re-enable the dead players
-        foreach (GameObject player in GameManager.inst.deadPlayers)
-        {
-            player.GetComponent<PlayerCube>().isDead = false;
-            player.GetComponent<PlayerCube>().playerMesh.SetActive(true);
-        }
-
-        // Destroy the two checkpoints
-        Destroy(GameManager.inst.playerOneSpawn);
-        Destroy(GameManager.inst.playerTwoSpawn);
-        GameManager.inst.deadPlayers.Clear(); 
-        isZooming = false;
-
-        GameManager.inst.StartCoroutine(GameManager.inst.SpawnLevel());
-
-        yield break;
     }
 }
--- Warp/Assets/Scripts/C#/Miguel'sScripts/PlayerCube.cs
+++ Warp/Assets/Scripts/C#/Miguel'sScripts/PlayerCube.cs
@@ -4,226 +4,37 @@ using UnityEngine;
 
 public class PlayerCube : MonoBehaviour
 {
-    [Header("Variables")]
+
     public bool isDead; // Used to determine if this player has died TO TRAPS
     public bool isFalling; // Check if this player died by FALLING OFF the map
-    public bool isDisabled = false; // Since movement is not physics based, need to disable player first before teleportion for it to work
-    public bool won = false; // Used to ensure win is only added once as the win function is under Update
-    public bool isFadingStarted = false;
-    public LayerMask whatIsGround;
-    public string player;
 
-    public float speed = 5f;
-    public float rotationRate = 60f; // This is only meant for rotating the aiming line of the player2, as aiming of player1 is based on mousePosition
-    public float fadeCountdown; // Max value of the countdown before aiming circle fades in/out
-    public float countdown;
-
-    public Vector3 offset;
-    Vector3 rot; // This is the desired rotation for our bullet spawning and aiming circle
-    Vector3 velocity;
-    
-
-    Color originalColour;
-    [Header("References")]
     public GameObject playerMesh;
     public GameObject corpse;
-    public GameObject bullet; // P1 and P2 have different bullets
-    public Transform bulletSpawn;
-    public Transform aimStart, aimEnd; // Store reference to start and end points of the aiming lines
-    public GameObject mark; // P1 and P2 have different marks
-    public GameObject aimCircle; // This is for changing the alpha of the aimCircle sprite
-    public GameObject aimCrosshair; // This is for rotating the crosshair so that it is still somewhat centred on player
 
     CameraController theCamera;
-    //Rigidbody rb;
-    CharacterController cc;
-    SpriteRenderer spriteRenderer;
 
     // Start is called before the first frame update
     void Start()
     {
         theCamera = FindObjectOfType<CameraController>();
-        cc = GetComponent<CharacterController>();
-        spriteRenderer = aimCircle.GetComponent<SpriteRenderer>(); // Get the sprite renderer of aimCircle
-
-        mark.transform.position = transform.position; // Set default position of mark to be player's position
-        originalColour = spriteRenderer.color; // Stores reference to the original colour of crosshair so we can go back to this color later
-
-        //offset = transform.rotation.eulerAngles; // Vector to apply when calculating rotation of character to turn towards
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (isDead)
+
+        if (isDead) // If the player has died, run the death method
         {
             PlayerDeath();
-
-            if (GameManager.inst.deadPlayers.Contains(gameObject)) return; // If this player is already part of the deadPlayers list, return
-            GameManager.inst.deadPlayers.Add(gameObject); // Add this to the list of deadPlayers
-            return;
-        }
-
-        if (!isDisabled)
-        {
-            UpdateMovement(); 
-            Aim();
-            UpdateAimingCircle();
-
-            // Shoot bullets
-            if (Input.GetButtonDown("Fire1 " + player)) 
-            {
-                SoundManager.instance.Play("Shoot"); // Play Shoot SFX
-                Instantiate(bullet, bulletSpawn.position, bulletSpawn.rotation);
-            }
-
-            // Place Marks that set teleport destination for enemy player
-            if (Input.GetButtonDown("Fire2 " + player)) 
-            {
-                mark.transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
-            }
-
-            // If this player is not dead, the dead player is not this object and the deadPlayers.Count is 0. deadPlayers != null does not work
-            if (!isDead && GameManager.inst.deadPlayers.Count != 0) // GameManager.inst.deadPlayer
-            {
-                if(won) return;
-
-                won = true; // Sets this to true so win method is called once
-                GameManager.inst.AddPlayerWin(gameObject); // Calls this method and declares which player won so a win for corresponding player is added
-            }
         }
     }
 
-    public void PlayerDeath() 
+    void PlayerDeath() // This is for death to traps
     {
-        if (!playerMesh.activeSelf) return; // If the player is alr dead, dont run rest of code
-
-        SoundManager.instance.Play("Death"); // Play Death SFX
         Instantiate(corpse, transform.position, transform.rotation); // Spawns the corpse at the location of the player
         playerMesh.SetActive(false); // Disables the player afterwards, giving off the illusion of player falling apart
-        cc.enabled = false; // Disable character controller to make movement of player between levels smoother
+
+        theCamera.target = this.gameObject; // Sets the target of the camera to the dead player
+        StartCoroutine(theCamera.DeathCam(this.gameObject));
     }
-
-    void UpdateMovement()
-    {
-        velocity += Physics.gravity * Time.deltaTime; // g ~= 10m/s^2; Causes player to fall
-        if (cc.isGrounded) velocity = Vector3.zero; // If player is grounded, set acceleration due to gravity to 0
-
-        Vector3 v = new Vector3(Input.GetAxis("Horizontal " + player), 0, Input.GetAxis("Vertical " + player));
-        //Debug.Log(player + ": " + Input.GetAxis("Horizontal " + player));
-        //Debug.Log(player + ": " + v);
-        if (v != Vector3.zero)
-        {
-            // Make the character smoothly rotate to face direction of movement
-            playerMesh.transform.rotation = Quaternion.Slerp(playerMesh.transform.rotation, Quaternion.LookRotation(v + offset), 0.25f); 
-        }
-
-        // If the character controller is enabled, move the character
-        if (cc.enabled)
-        {
-            cc.Move((transform.TransformDirection(v.normalized) * speed + velocity) * Time.deltaTime); // Moves player using inputs
-        }
-    }
-
-    void Aim()
-    {
-        if (player == "P1") // Aiming for player 1
-        {
-            Camera camera = FindObjectOfType<Camera>();
-            //Vector3 mouse = camera.ScreenToWorldPoint(Input.mousePosition); This won't work because screenToWorldPoint is not meant for 3D, only has x and y
-
-            Ray r = Camera.main.ScreenPointToRay(Input.mousePosition); // Shoots ray from mousePosition in camera into world space
-            RaycastHit hit;
-            if (Physics.Raycast(r, out hit, Mathf.Infinity, whatIsGround))
-            {
-                Vector3 h = new Vector3(hit.point.x, 0.5f, hit.point.z); // Offset the y of hit so that hit.point and bulletSpawn have the same y value
-
-                // Find vector I need to rotate such that forward Vector of bulletSpawn is pointed towards h
-                rot = Quaternion.LookRotation(h - bulletSpawn.transform.position).eulerAngles; 
-
-                rot.x = rot.z = 0; // Prevents rotation in x- or z-axis so bullets do not go into the ground; bulletSpawn only rotates in y-axis
-                bulletSpawn.transform.rotation = Quaternion.Euler(rot); // Finally, set the new rotation of bulletSpawn
-
-
-                // Check if there is any mouse movement, this will be used by UpdateAimCircle
-                if (Input.GetAxis("Mouse X") != 0 || Input.GetAxis("Mouse Y") != 0)
-                {
-                    countdown = fadeCountdown;
-                    isFadingStarted = false;
-                }
-            }
-        }
-        else if (player == "P2") // Aiming for player 2
-        {
-            if (Input.GetButton("ClockwiseAim"))
-            {
-                bulletSpawn.transform.Rotate(0, rotationRate * Time.deltaTime, 0); // Aim clockwise at 180 degrees/s
-                countdown = fadeCountdown;
-                isFadingStarted = false;
-            }
-            
-            if (Input.GetButton("CounterclockwiseAim"))
-            {
-                bulletSpawn.transform.Rotate(0, -rotationRate * Time.deltaTime, 0); // Aim counterclockwise at 180 degrees/s
-                countdown = fadeCountdown;
-                isFadingStarted = false;
-            }
-        }
-    }
-
-    void UpdateAimingCircle()
-    {
-
-        countdown -= Time.deltaTime;
-        if (countdown <= 0) // When countdown <= zero, the aimCircle will fade out
-        {
-            isFadingStarted = false;
-            //StartCoroutine(FadeAimingCircle());
-            StartCoroutine(FadeTo(0.0f));
-        }
-        else // When countdown is above 0, which is when the player is currently aiming, make aimCrosshair appear again
-        {
-            spriteRenderer.color = originalColour;
-        }
-
-        if (player == "P1")
-        {
-            // Apply a 90-degree counterclockwise rotation around world y-axis to rot so bar of crosshair aligns with trajectory of projectiles
-            Quaternion crosshairRotation = Quaternion.AngleAxis(-90, Vector3.up) * Quaternion.Euler(rot); 
-
-            // Set the rotation
-            aimCrosshair.transform.rotation = crosshairRotation;
-        }
-        if (player == "P2")
-        {
-            if (Input.GetButton("ClockwiseAim"))
-            {
-                aimCrosshair.transform.Rotate(0, rotationRate * Time.deltaTime, 0); // Aim clockwise at 180 degrees/s
-
-            }
-
-            if (Input.GetButton("CounterclockwiseAim"))
-            {
-                aimCrosshair.transform.Rotate(0, -rotationRate * Time.deltaTime, 0); // Aim counterclockwise at 180 degrees/s
-
-            }
-        }
-    }
-
-    IEnumerator FadeTo(float aValue)
-    {
-        float alpha = spriteRenderer.color.a;
-
-        while (isFadingStarted == true) yield break;
-        isFadingStarted = true; // Prevents this coroutine from constantly running
-
-        for (float t = 0.0f; t < 1.0f; t += Time.deltaTime)
-        {
-            Color newColor = new Color(originalColour.r, originalColour.g, originalColour.b, Mathf.Lerp(alpha, aValue, t));
-            spriteRenderer.color = newColor;
-
-            yield return null;
-        }
-        yield break;
-    } 
 }
